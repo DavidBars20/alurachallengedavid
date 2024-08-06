837c748f9dbea6ff11473009a9334e6e06444ce2
DavidBars20 <158859587+DavidBars20@users.noreply.github.com>
2024-08-06 13:03:28 -0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -7,9 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
             "o": "ober",
             "u": "ufat"
         };
-
         let textoCodificado = "";
-
         for (let caractere of texto.toLowerCase()) {
             if (mapaSubstituicoes.hasOwnProperty(caractere)) {
                 textoCodificado += mapaSubstituicoes[caractere];
@@ -17,10 +15,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 textoCodificado += caractere;
             }
         }
-
         return textoCodificado;
     }
-
     function decodificarTexto(textoCodificado) {
         const mapaDecodificacao = {
             "ai": "a",
@@ -29,33 +25,25 @@ document.addEventListener("DOMContentLoaded", () => {
             "ober": "o",
             "ufat": "u"
         };
-
         const substituicoes = Object.keys(mapaDecodificacao).sort((a, b) => b.length - a.length);
-
         let textoDecodificado = textoCodificado;
-
         for (let chave of substituicoes) {
             const valorOriginal = mapaDecodificacao[chave];
             const regex = new RegExp(chave, 'g');
             textoDecodificado = textoDecodificado.replace(regex, valorOriginal);
         }
-
         return textoDecodificado;
     }
-
     function mostrarTextoNaTela(seletor, texto) {
         let elemento = document.querySelector(seletor);
         elemento.textContent = texto;
     }
-
     function esconderElemento(id) {
         document.getElementById(id).style.display = "none";
     }
-
     function mostrarElemento(id) {
         document.getElementById(id).style.display = "block";
     }
-
     const botaoCriptografar = document.querySelector("#criptografar");
     botaoCriptografar.addEventListener("click", (evento) => {
         evento.preventDefault();
@@ -67,7 +55,6 @@ document.addEventListener("DOMContentLoaded", () => {
         mostrarElemento('resultado');
         esconderElemento('imagem-mensagem');
     });
-
     const botaoDescriptografar = document.querySelector("#descriptografar");
     botaoDescriptografar.addEventListener("click", (evento) => {
         evento.preventDefault();
@@ -79,7 +66,6 @@ document.addEventListener("DOMContentLoaded", () => {
         mostrarElemento('resultado');
         esconderElemento('imagem-mensagem');
     });
-
     document.getElementById('btn_copiar').addEventListener('click', copiarParaClipboard);
     async function copiarParaClipboard() {
         let texto = document.querySelector("#resultado").textContent;
